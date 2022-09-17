922730e04316b8e8ce56d188e89ad7ccbd87b326
Blake C <bcortri1@kent.edu>
2022-09-17 01:55:05 -0400

    part 2 & 3 complete

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 console.log("Let's get this party started!");
-api_key="MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym";
 
+//Grabs giphy search data and returns an array of objects
+function gSearch(){
+    let response = axios.get(`http://api.giphy.com/v1/gifs/search?q=${$("input").val()}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`);
+    return response;
+}
+//Takes a promised array and appends random url to gif-container
+async function appendGif(response){
+    response = await response;
+    console.log(response)
+    let rand = Math.floor(Math.random()* 50);
+    let gifUrl= response.data.data[rand].images.original.url;
+    $("#gif-container").append($("<img>").attr("src", gifUrl));
+
+}
 
 $("#sBtn").on("click", function(evt){
     evt.preventDefault();
-    gSearch();
+    appendGif(gSearch());
 });
 
 $("#rBtn").on("click", function(evt){
     evt.preventDefault();
     $("#gif-container").empty();
 });
-
-
-async function gSearch(){
-    let response = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${$("input").val()}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`);
-    console.log("got", response);
-}
-
